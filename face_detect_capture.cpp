d12b1cb5678c4baa6a7ef81e45cc31340f3570a2
ryuspace <40001772+ryuspace@users.noreply.github.com>
2020-01-10 19:40:37 +0900

    Update face_detect_capture.cpp

--- face_detect_capture.cpp
+++ face_detect_capture.cpp
@@ -1,4 +1,5 @@
-//얼굴이 인식되면 캡쳐되게 한다. 하지만 얼굴 인식하는 성능이 좋은 것 같지 않다.
+//눈이 인식되면 현재 웹캠을 캡쳐한다. 눈을 인식하는 것이 얼굴을 인식하는 것 보다 검출률이 더 좋다.
+//Issue : 조금 더 얼굴인식을 정확하게 할 방법을 고려해보자.
 #include <iostream>
 #include <opencv2/opencv.hpp>
 #include <stdlib.h>
@@ -25,8 +26,8 @@ int main(int argc, char* argv[])
     namedWindow("webcam", 1);
     CascadeClassifier face_classifier, eye_classifier;
 
-    face_classifier.load("C:/opencv/sources/data/haarcascades/haarcascade_frontalface_default.xml");
-    //eye_classifier.load("C:/opencv/sources/data/haarcascades/haarcascade_eye.xml");
+    //face_classifier.load("C:/opencv/sources/data/haarcascades/haarcascade_frontalface_default.xml");
+    eye_classifier.load("C:/opencv/sources/data/haarcascades/haarcascade_eye.xml");
     Mat frame;
     while (1) {
 
@@ -46,17 +47,33 @@ int main(int argc, char* argv[])
                 cvtColor(frame, grayframe, cv::COLOR_BGR2GRAY);
                 equalizeHist(grayframe, grayframe);
                 vector<Rect> faces,eyes;
-                face_classifier.detectMultiScale(grayframe, faces,
-                    1.1, // increase search scale by 10% each pass
-                    3,   // merge groups of three detections
-                    0,
-                    Size(30, 30));
-                //eye_classifier.detectMultiScale(grayframe, eyes,
+                //face_classifier.detectMultiScale(grayframe, faces,
                 //    1.1, // increase search scale by 10% each pass
                 //    3,   // merge groups of three detections
                 //    0,
                 //    Size(30, 30));
-                if (faces.size() == 1)
+
+                eye_classifier.detectMultiScale(grayframe, eyes,
+                    1.1, // increase search scale by 10% each pass
+                    3,   // merge groups of three detections
+                    0,
+                    Size(30, 30));
+                if (eyes.size() == 2)
+                {
+                    capture.read(frame);
+                    sprintf(buf, "c:/temp/img_%06d.jpg", index);
+                    cout << buf << '\n';
+                    imwrite(buf, frame);
+                    index++;
+                    for (int i = 0; i < eyes.size(); i++) {
+
+                        Point lb(eyes[i].x + eyes[i].width, eyes[i].y + eyes[i].height);
+                        Point tr(eyes[i].x, eyes[i].y);
+                        rectangle(frame, tr, lb, Scalar(0, 0, 255));
+
+                    }
+                }
+                /*if (faces.size() == 1)
                 {
                     capture.read(frame);
                     sprintf(buf, "c:/temp/img_%06d.jpg", index);
@@ -70,18 +87,8 @@ int main(int argc, char* argv[])
                         rectangle(frame, tr, lb, Scalar(0, 255, 0));
 
                     }
-                }
+                }*/
                 
-                //if (eyes.size() == 2)
-                //{
-                //    for (int i = 0; i < eyes.size(); i++) {
-
-                //        Point lb(eyes[i].x + eyes[i].width, eyes[i].y + eyes[i].height);
-                //        Point tr(eyes[i].x, eyes[i].y);
-                //        rectangle(frame, tr, lb, Scalar(0, 0, 255));
-
-                //    }
-                //}
                 imshow("webcam", frame);
                 //writer.write(frame);
 
